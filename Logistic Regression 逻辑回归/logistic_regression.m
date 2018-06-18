bdc743cda07dd15802f2d50be1f22dfe01da4d49
SACxAoi <sacxaoi@gmail.com>
2018-06-18 18:05:31 +0800

    Add plot function

--- Logistic Regression 逻辑回归/logistic_regression.m
+++ Logistic Regression 逻辑回归/logistic_regression.m
@@ -13,7 +13,22 @@ X = [x0 x1 x2];
 %code above for future plotting
 %unfinished
 
+x1_1 = x1;
+x1_0 = x1;
+x1_1(y == 0, :) = [];
+x1_0(y == 1, :) = [];
+x2_1 = x2;
+x2_0 = x2;
+x2_1(y == 0, :) = [];
+x2_0(y == 1, :) = [];
+
 options = optimset('GradObj', 'On', 'MaxIter', 500);
 theta = zeros(n, 1);
 [opt_theta, function_value, exit_flag] = fminunc(@cost_J, theta, options);
+
+hold on
+scatter(x1_1, x2_1, 'r')
+scatter(x1_0, x2_0, 'b')
+plot(x1, (opt_theta(1)-0.5+opt_theta(2)*x1)/(-opt_theta(3)), 'k')
+%decision boundary = 0.5
 
